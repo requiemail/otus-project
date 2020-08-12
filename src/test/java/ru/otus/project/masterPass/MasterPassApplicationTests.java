4f59c780f9cb7ee8393dc20178646df6ca2f07b1
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-08-12 23:05:33 +0300

    Инициализирован проект Spring Boot, размечена файловая структура проекта

---
+++ src/test/java/ru/otus/project/masterPass/MasterPassApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.project.masterPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
