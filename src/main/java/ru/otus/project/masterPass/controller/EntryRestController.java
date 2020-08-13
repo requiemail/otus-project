4e0a290b53b062a6168d9a98599b7be13567e6d2
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-08-13 22:56:37 +0300

    Контроллер дописан до CRUD

--- src/main/java/ru/otus/project/masterPass/controller/EntryRestController.java
+++ src/main/java/ru/otus/project/masterPass/controller/EntryRestController.java
@@ -1,8 +1,11 @@
 package ru.otus.project.masterPass.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,7 @@ import ru.otus.project.masterPass.domain.Entry;
 import ru.otus.project.masterPass.service.EncryptDecryptService;
 import ru.otus.project.masterPass.service.EntryService;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +35,16 @@ public class EntryRestController {
         return entryService.getAll().stream()
                 .map(encryptDecryptService::decrypt)
                 .collect(Collectors.toList());
+    }
+
+    @PutMapping("/{id}")
+    public Entry encryptAndUpdate(@PathVariable("id") Long id, @RequestBody Entry entry) {
+        return entryService.save(encryptDecryptService.encrypt(entry));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        entryService.deleteById(id);
     }
 
 }
