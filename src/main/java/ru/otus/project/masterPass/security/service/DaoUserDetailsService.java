58991cc50bfa7c9530fe979976e626b640cb6403
a17600765 <Vetchinnikov.P.E@sberbank.ru>
2020-08-20 18:45:45 +0300

    Исправлены импорты

--- src/main/java/ru/otus/project/masterPass/security/service/DaoUserDetailsService.java
+++ src/main/java/ru/otus/project/masterPass/security/service/DaoUserDetailsService.java
@@ -5,8 +5,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.otus.project.masterPass.domain.User;
-import ru.otus.project.masterPass.repository.UserRepository;
+import ru.otus.project.masterPass.security.domain.User;
+import ru.otus.project.masterPass.security.repository.UserRepository;
 
 
 @RequiredArgsConstructor
